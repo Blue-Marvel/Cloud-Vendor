be52c19c12edbf3d35a57cb9c50b724c94936be3
marvel <marvellousogbo0@gmail.com>
2024-01-17 00:58:02 +0100

    first commit

---
+++ src/main/java/com/springboot/app/CloudVendorAPIService.java
@@ -0,0 +1,49 @@
+package com.springboot.app;
+
+import com.springboot.app.model.CloudVendor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/cloud-vendor")
+public class CloudVendorAPIService {
+
+    CloudVendor cloudVendor;
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId) {
+
+        if (cloudVendor == null) {
+            return null;
+        }
+
+        return cloudVendor;
+    }
+
+    @PostMapping()
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud vendor created successfully";
+    }
+
+    @PutMapping()
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud vendor updated successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId) {
+        this.cloudVendor = null;
+        return "Cloud vendor deleted successfully";
+
+    }
+
+}
